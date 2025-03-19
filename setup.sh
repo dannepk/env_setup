4d90eaf7c4bdf4fb62acb050905eb8f7f1d42abd
dannepk <daniel92920@gmail.com>
2025-03-19 14:35:09 +0100

    setup script

---
+++ setup.sh
@@ -0,0 +1,10 @@
+sudo pacman -S neofetch
+sudo pacman -S neovim
+sudo pacman -S obsidian
+sudo pacman -S kitty
+sudo pacman -S yazi
+sudo pacman -S chromium
+sudo pacman -S git
+ssh-keygen -t rsa -b 4096 -C "daniel92920@gmail.com"
+cat ~/.ssh/id_rsa.pub 
+echo "add key to github"
