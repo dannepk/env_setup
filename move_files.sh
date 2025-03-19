b97b6c1092e92d24a1ed0c50cc5dd67699353af3
dannepk <daniel92920@gmail.com>
2025-03-19 15:49:16 +0100

    Added full screen shortcut

---
+++ move_files.sh
@@ -0,0 +1,4 @@
+cp -r kitty ../.config/
+cp -r nvim ../.config/
+cp starship.toml ../.config/
+cp -r wallpapers/ ../Pictures/
--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ sudo pacman -S neofetch neovim obsidian kitty yazi chromium git starship
 ssh-keygen -t rsa -b 4096 -C "daniel92920@gmail.com"
 cat ~/.ssh/id_rsa.pub 
 echo "eval "$(starship init bash)"" >> ~/.bashrc 
-echo "add key to github"
+echo "add key to github" 
